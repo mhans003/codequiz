92a38d3fc8946c7df619e054528f98f3602d610a
mhans003 <michaeledwardhanson@gmail.com>
2020-11-08 15:41:52 -0800

    Testing Heroku: added check for process.env.DATABASE_URL in models index.

--- models/index.js
+++ models/index.js
@@ -9,7 +9,14 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
+// checks if env is Heroku, if so, sets sequelize to utilize the database hosted on heroku
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres'
+  }); 
+} else if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
