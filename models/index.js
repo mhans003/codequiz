7c2fa89f5baa3bd041934041cead4ba679304032
mhans003 <michaeledwardhanson@gmail.com>
2020-11-08 16:10:17 -0800

    used database_url to test heroku in model index

--- models/index.js
+++ models/index.js
@@ -9,9 +9,9 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+if (process.env.DATABASE_URL) {
   // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
     port:     match[4],
